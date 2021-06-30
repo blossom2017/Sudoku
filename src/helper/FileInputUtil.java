c4bc79879836d4b28765d0189ab1da3b4d1750e8
maitrishastri <maitrishastri2016@gmail.com>
2021-06-30 19:56:46 +0530

    Added functionality to read input from a text file.(To be tested)

---
+++ src/helper/FileInputUtil.java
@@ -0,0 +1,43 @@
+package helper;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.IllegalArgumentException;
+
+/**
+ * Provides helper methods to create sudoku input from a text file
+ *
+ * @author maitrishastri
+ */
+public class FileInputUtil {
+    private int[][] readFile(String filename) throws IllegalArgumentException {
+        BufferedReader bf;
+        int[][] sudokuBoard;
+
+        try {
+            bf = new BufferedReader(new FileReader(filename));
+            try {
+                int numberOfLines = Integer.valueOf(bf.readLine());
+                sudokuBoard = new int[numberOfLines][numberOfLines];
+                for (int i = 0; i < numberOfLines; i++) {
+                    //split by spaces into an array of numbers.
+                    String temp[] = bf.readLine().split("\\s+");
+                    if (temp.length != numberOfLines) {
+                        throw new IllegalArgumentException("The sudoku given is invalid as the number of rows don't match the number of columns.");
+                    }
+                    for (int j = 0; j < temp.length; j++) {
+                        sudokuBoard[i][j] = Integer.valueOf(temp[j]);
+                    }
+                }
+                return sudokuBoard; //return the board once file has been read.
+            } catch (IOException e) {
+                System.out.println("Cannot read file " + filename);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot find file " + filename);
+        }
+        return null;
+    }
+}
---
+++ test/java/helper/FileInputUtilTest.java
@@ -0,0 +1,12 @@
+package helper;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileInputUtilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
