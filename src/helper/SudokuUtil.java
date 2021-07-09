83be928b84ee19cf97df9874be9432b6b4f1e492
maitrishastri <maitrishastri2016@gmail.com>
2021-07-09 22:03:06 +0530

    Adding a function to check if sudoku is correctly filled or not.

--- src/helper/SudokuUtil.java
+++ src/helper/SudokuUtil.java
@@ -1,5 +1,8 @@
 package helper;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Provides helper methods required to solve a sudoku.
  *
@@ -8,6 +11,7 @@ package helper;
 public class SudokuUtil {
     /**
      * Returns true if the sudoku grid is completely filled and false otherwise.
+     *
      * @param sudokuGrid 2d Array representation of sudoku
      * @return true/false
      */
@@ -24,12 +28,83 @@ public class SudokuUtil {
     }
 
     /**
+     * Returns true if the given sudoku satisfies row and column constraints
+     *
+     * @param sudokuGrid 2d Array representation of sudoku
+     * @return true/false
+     */
+    public boolean isCorrectRowAndColumn(int[][] sudokuGrid) {
+        int row = sudokuGrid.length;
+        int col = sudokuGrid[0].length;
+        for (int i = 0; i < row; i++) {
+            HashSet<Integer> str = new HashSet<>();
+            HashSet<Integer> stc = new HashSet<>();
+            for (int j = 0; j < col; j++) {
+                if (stc.contains(sudokuGrid[i][j]))
+                    return false;
+                stc.add(sudokuGrid[i][j]);
+            }
+            for (int j = 0; j < col; j++) {
+                if (str.contains(sudokuGrid[j][i]))
+                    return false;
+                str.add(sudokuGrid[j][i]);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if the given sudoku satisfies sub-square constraints
+     *
+     * @param sudokuGrid 2d Array representation of sudoku
+     * @return true/false
+     */
+    public boolean isCorrectSubSquare(int[][] sudokuGrid) {
+        int col = sudokuGrid.length;
+        int d = (int) Math.sqrt(col);
+        for (int i = 0; i < col; i += d) {
+            for (int j = 0; j < col; j += d) {
+                HashSet<Integer> st = new HashSet<>();
+                for (int k = 0; k < d; k++) {
+                    for (int l = 0; l < d; l++) {
+                        if (st.contains(sudokuGrid[i + k][j + l])) return false;
+                        st.add(sudokuGrid[i + k][j + l]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if the given sudoku is complete and correct.
+     *
+     * @param sudokuGrid
+     * @return true/false
+     */
+    public boolean isCorrectSudoku(int[][] sudokuGrid) {
+        int row = sudokuGrid.length;
+        if (row == 0) return true;
+        int col = sudokuGrid[0].length;
+        if (row != col) {
+            throw new IllegalArgumentException("The number of rows should be equal to number of columns");
+        }
+        if (!isPerfectSquare(row)) {
+            throw new UnsupportedOperationException("Only perfect square sudoku boards are supported.");
+        }
+        return isCompletedSudoku(sudokuGrid)
+                && isCorrectRowAndColumn(sudokuGrid)
+                && isCorrectSubSquare(sudokuGrid);
+    }
+
+    /**
      * Returns true if the given move is valid i.e satisfies all row constraints,
      * column constraints and sub-square constraints.
+     *
      * @param sudokuGrid 2d Array representation of sudoku
-     * @param row Row number where the number will be filled
-     * @param column Column number where the number will be filled
-     * @param num Number
+     * @param row        Row number where the number will be filled
+     * @param column     Column number where the number will be filled
+     * @param num        Number
      * @return True/False
      */
     public boolean isValid(int[][] sudokuGrid, int row, int column, int num) {
@@ -41,10 +116,11 @@ public class SudokuUtil {
 
     /**
      * Returns true if the given move satisfies row constraint.
+     *
      * @param sudokuGrid 2d Array representation of sudoku
-     * @param row Row number where the number will be filled
-     * @param col Column number where the number will be filled
-     * @param num Number
+     * @param row        Row number where the number will be filled
+     * @param col        Column number where the number will be filled
+     * @param num        Number
      * @return True/False
      */
     public boolean isRowValid(int[][] sudokuGrid, int row, int col, int num) {
@@ -58,10 +134,11 @@ public class SudokuUtil {
 
     /**
      * Returns true if the given move satisfies column constraint.
+     *
      * @param sudokuGrid 2d Array representation of sudoku
-     * @param row Row number where the number will be filled
-     * @param col Column number where the number will be filled
-     * @param num Number
+     * @param row        Row number where the number will be filled
+     * @param col        Column number where the number will be filled
+     * @param num        Number
      * @return True/False
      */
     public boolean isColumnValid(int[][] sudokuGrid, int row, int col, int num) {
@@ -75,10 +152,11 @@ public class SudokuUtil {
 
     /**
      * Returns true if the given move satisfies sub-square constraint.
+     *
      * @param sudokuGrid 2d Array representation of sudoku
-     * @param row Row number where the number will be filled
-     * @param col Column number where the number will be filled
-     * @param num Number
+     * @param row        Row number where the number will be filled
+     * @param col        Column number where the number will be filled
+     * @param num        Number
      * @return True/False
      */
     public boolean isSubSquareValid(int[][] sudokuGrid, int row, int col, int num) {
@@ -97,6 +175,7 @@ public class SudokuUtil {
 
     /**
      * Utility to print the board
+     *
      * @param sudokuGrid 2d Array representation of sudoku
      */
     public void printBoard(int[][] sudokuGrid) {
@@ -111,6 +190,7 @@ public class SudokuUtil {
 
     /**
      * Returns true if the number is perfect square.
+     *
      * @param num Size of the sudoku board
      * @return True/False
      */
